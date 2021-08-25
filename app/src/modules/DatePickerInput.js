8a1c82e4b900df4f3f64d426ac43b44910ea93fc
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-08-25 19:37:03 +0200

    clean up code, remove comments

--- app/src/modules/DatePickerInput.js
+++ app/src/modules/DatePickerInput.js
@@ -1,10 +1,7 @@
 import React, { useEffect } from "react";
 import { Input } from "../styles/home";
 import $ from "jquery";
-// why import the below as is?
 import "jquery-datetimepicker";
-// why do I have to explicitely import this?
-// how do I know what to import?
 import "jquery-datetimepicker/build/jquery.datetimepicker.min.css";
 
 function DatePickerInput(props) {
@@ -21,14 +18,12 @@ function DatePickerInput(props) {
       timepicker: false,
       format: "m/d/Y",
       onChangeDateTime: function (dp, $input) {
-        // const e = { target: { id: id, value: $input.val() } };
         props.onChange({ target: { id: id, value: $input.val() } });
       },
     });
   });
 
   return (
-    // wieso funktioniert das hier mit readOnly? Nutzer kann trotzdem noch Datum verändern.
     <Input id={props.id} type="text" ref={elmt} value={props.value} readOnly />
   );
 }
--- app/src/modules/SelectInputField.js
+++ app/src/modules/SelectInputField.js
@@ -1,13 +1,5 @@
 import React, { useEffect } from "react";
 import $ from "jquery";
-// import "jquery-ui/themes/base/core.css";
-// import "jquery-ui/themes/base/theme.css";
-// import "jquery-ui/themes/base/selectmenu.css";
-// import "jquery-ui/ui/widget";
-// import "jquery-ui/ui/position";
-// import "jquery-ui/ui/core";
-// import "jquery-ui/ui/widgets/menu";
-// import "jquery-ui/ui/widgets/selectmenu";
 import "jquery-ui-bundle";
 import "jquery-ui-bundle/jquery-ui.css";
 
